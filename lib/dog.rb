ddffc25509ed30d2b022c3a76e12b88d0d6b4674
Shangni Hu <shangnihu@gmail.com>
2018-06-13 23:15:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,4 +25,15 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+    end
+  end
+
 end
